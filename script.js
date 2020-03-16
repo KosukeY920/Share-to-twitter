53da748651716898afb16136b1e0527953661a9d
KosukeY0920 <yokomaekatuo@gmail.com>
2020-03-17 02:48:53 +0900

    ADD:genURL()

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var template = 'https://twitter.com/intent/tweet?';
-  var escaped_titile = encodeURIComponent(tab.title);
-  var title = 'text=' + escaped_titile;
-  var escaped_url = encodeURIComponent(tab.url);
-  var url = '&url=' + escaped_url;
-  console.log(template + title + url);
-  window.open(template + title + url , 'newtab');
+  genURL(tab);
 });
 
 chrome.contextMenus.create({
   title : "Share to twitter",
   contexts : ["all"],
   type : "normal",
-  onclick : function(info){
-    chrome.tabs.getSelected(null, function(tabs){
-      var template = 'https://twitter.com/intent/tweet?';
-      var escaped_titile = encodeURIComponent(tabs.title);
-      var title = 'text=' + escaped_titile;
-      var escaped_url = encodeURIComponent(tabs.url);
-      var url = '&url=' + escaped_url;
-      console.log(template + title + url);
-      window.open(template + title + url , 'newtab');
-    });
+  onclick : function(info, tab){
+    genURL(tab);
   }
 })
 
+function genURL(tab){
+  var template = 'https://twitter.com/intent/tweet?';
+  var title = 'text=' + encodeURIComponent(tab.title);
+  var url = '&url=' + encodeURIComponent(tab.url);
+  console.log(template + title + url);
+  window.open(template + title + url , 'newtab');
+}
